3c1941c2f5216b49f02c4ce583892bb55e905d01
Maxi Gimenez <gimenez.maxi@gmail.com>
2020-07-19 18:33:53 +0200

    fix: formatting

--- components/not-found.tsx
+++ components/not-found.tsx
@@ -5,8 +5,7 @@ import config from '@/config';
 export const NotFound = () => (
   <>
     <Head>
-      <title>{config
-      .title}</title>
+      <title>{config.title}</title>
     </Head>
     <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
       <h1 className="mr-3 pr-3 align-top border-right inline-block align-content-center">404</h1>
